af2f9390703950737fe584a08958573878824796
arafatai388 <arafat.cseai@gmail.com>
2026-07-18 17:39:52 +0600

    Learn JavaScript array length property

---
+++ length.js
@@ -0,0 +1,14 @@
+const fruits = [
+    "Mango",
+    "Lychee",
+    "JackFruit",
+    "Papaya",
+    "Grape",
+    "Grapefruit"
+];
+
+const totalFruits = fruits.length;
+
+console.log("Total Fruits:", totalFruits);
+console.log("Fruits:", fruits);
+console.log("Last Fruit:", fruits[fruits.length - 1]);
