b4af0b9bfe0c2b53a3af79b5291e316aa33b22cb
arafatai388 <arafat.cseai@gmail.com>
2026-07-18 18:00:53 +0600

    Learn JavaScript array push method

---
+++ push.js
@@ -0,0 +1,15 @@
+const friends = [
+    'balam',
+    'kalam',
+    'salam',
+    'gelam',
+    'khailam'
+];
+
+console.log("Before Push:", friends);
+
+friends.push('ghumailam');
+friends.push('mobailam');
+
+console.log("After Push:", friends);
+console.log("Total Friends:", friends.length);
