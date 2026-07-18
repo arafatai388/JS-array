f9b31b7e1bd81d555fb4db0e5cf4a1cb0d68d242
arafatai388 <arafat.cseai@gmail.com>
2026-07-19 01:21:40 +0600

    Learn JavaScript for loop with array index

---
+++ for.js
@@ -0,0 +1,9 @@
+const numbers = [87, 54, 12, 63, 41, 87, 51];
+
+const length = numbers.length;
+
+console.log("Array Length:", length);
+
+for (let i = 0; i < length; i++) {
+    console.log(`Index ${i} → Value ${numbers[i]}`);
+}
