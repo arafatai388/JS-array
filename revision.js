cfdde1860116218e64d89e8b80f49180b8a9c915
arafatai388 <arafat.cseai@gmail.com>
2026-07-19 01:13:14 +0600

    Revise JavaScript fundamentals

---
+++ revision.js
@@ -0,0 +1,16 @@
+// Variables
+const price = 50;
+const studentName = "Jobbbar Khan";
+const isRich = true;
+
+// Conditional
+if (studentName === "Jobbbar Khan") {
+    console.log("Student Found");
+} else {
+    console.log("Student Not Found");
+}
+
+// Loop
+for (let i = 100; i < 125; i++) {
+    console.log(i);
+}
