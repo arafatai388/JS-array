ff8fe282a4d59b85a8bffecf8eca36e8ee5f67cf
arafatai388 <arafat.cseai@gmail.com>
2026-07-19 01:20:01 +0600

    Learn JavaScript for...of loop

---
+++ loop.js
@@ -0,0 +1,20 @@
+const numbers = [87, 54, 12, 63, 41];
+
+console.log("Numbers:");
+
+for (const number of numbers) {
+    console.log(number);
+}
+
+const players = [
+    "Jamir",
+    "Khamir",
+    "Namir",
+    "Damir"
+];
+
+console.log("Players:");
+
+for (const player of players) {
+    console.log(player);
+}
