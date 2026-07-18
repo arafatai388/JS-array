da543ab773c50911c50b22aba4ffceafdade8bf0
arafatai388 <arafat.cseai@gmail.com>
2026-07-18 18:11:59 +0600

    Learn JavaScript array shift method

---
+++ shift.js
@@ -0,0 +1,11 @@
+const numbers = [12, 45, 145, 25, 78, 89, 94];
+
+console.log("Before Shift:", numbers);
+
+const removedNumber = numbers.shift();
+
+console.log("Removed Number:", removedNumber);
+
+console.log("After Shift:", numbers);
+
+console.log("Current Length:", numbers.length);
