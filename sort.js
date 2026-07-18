69c0af5ddedee072db410fc76f0991164ff90fec
arafatai388 <arafat.cseai@gmail.com>
2026-07-19 01:17:47 +0600

    Learn JavaScript array sort method

---
+++ sort.js
@@ -0,0 +1,17 @@
+const numbers = [5, 2, 9, 7, 1, 3];
+
+numbers.sort((a, b) => a - b);
+
+console.log("Sorted Numbers:", numbers);
+
+const fruits = [
+    "mango",
+    "jackfruit",
+    "guava",
+    "papaya",
+    "banana"
+];
+
+fruits.sort();
+
+console.log("Sorted Fruits:", fruits);
