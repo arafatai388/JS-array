cd5286b4c7f34ca340230e2e17677d7b4bdf437d
arafatai388 <arafat.cseai@gmail.com>
2026-07-18 17:32:22 +0600

    Learn JavaScript array creation and data types

---
+++ array.js
@@ -0,0 +1,27 @@
+// Individual variables
+const player1 = 45;
+const player2 = 98;
+const player3 = 56;
+
+// Arrays
+const teamScores = [123, 89, 45, 69, 78];
+
+const playerNames = [
+    'jamir',
+    'khamir',
+    'namir',
+    'damir'
+];
+
+const richStudents = [
+    true,
+    false,
+    true,
+    false,
+    false,
+    false
+];
+
+console.log("Team Scores:", teamScores);
+console.log("Player Names:", playerNames);
+console.log("Rich Students:", richStudents);
